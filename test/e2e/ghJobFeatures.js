4404b749ad156a65e5730d2f6c5daabc0d47e857
Paul <paulalexrees@gmail.com>
2016-04-21 12:10:53 +0100

    search field and button done

--- test/e2e/ghJobFeatures.js
+++ test/e2e/ghJobFeatures.js
@@ -2,5 +2,18 @@ describe('ghJob', function() {
   it ('has a title', function(){
     browser.get('/');
     expect(browser.getTitle()).toEqual("Title");
-  })
-})
+  });
+
+  it('displays a list of github users', function() {
+
+
+  });
+
+  it ('has a search field with a search button', function() {
+    browser.get('/');
+    var field = element(by.id('searchfield'));
+    var button = element(by.id('searchbutton'));
+    expect(field.isPresent()).toBeTruthy();
+    expect(button.isPresent()).toBeTruthy();
+  });
+});
